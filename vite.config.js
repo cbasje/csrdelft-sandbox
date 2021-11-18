06e6e6e3b64f4c008bf4bab288c69bc083b9a6d0
cbasje <45468731+cbasje@users.noreply.github.com>
2021-11-18 13:13:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,22 @@ import handlebars from 'vite-plugin-handlebars';
 
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+module.exports = defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				forum: resolve(__dirname, 'forum.html'),
+				'forum-topic': resolve(__dirname, 'forum-topic.html'),
+			},
+		},
+	},
 	plugins: [
 		handlebars({
 			partialDirectory: resolve(__dirname, 'partials'),
 			context: {
 				title: 'Hello, world!',
-				'forum-messages': [
-					'Hallo',
-					'Een item',
-					'Bericht!'
-				]
+				'forum-messages': ['Hallo', 'Een item', 'Bericht!'],
 			},
 		}),
 	],
