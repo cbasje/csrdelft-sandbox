7016c7115f107b7f220cf1494eaf40c1d5b5ccc5
cbasje <45468731+cbasje@users.noreply.github.com>
2021-11-18 12:58:45 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		handlebars({
+			partialDirectory: resolve(__dirname, 'partials'),
+			context: {
+				title: 'Hello, world!',
+				'forum-messages': [
+					'Hallo',
+					'Een item',
+					'Bericht!'
+				]
+			},
+		}),
+	],
+});
